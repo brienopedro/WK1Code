85b0010ac9cc043f82a8c1a204bf5a67bf6fd737
Pedro Brieno <brienopedro@gmail.com>
2020-06-04 02:54:41 -0700

    just added necessary file for this weeks assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+var pizza = 5.00;
+var soda = 1.99;
+
+var creditCard = 122.48;
+var cash = 40.00;
+
+var guyFriends = 13;
+var girlFriends = 12;
+
+var age = 25;
+
+var firstName = "Pedro";
+var lastName = 'Brieno';
+var middleInitial = "A.";
+
+console.log ('Hello World!');
+
+console.log (`My new balance is ${cash - [pizza * 3 + soda * 5]} after buying three pizza's and five sodas from Little Caesars`)
+
+console.log (`The number of friends I have made each year is ${[guyFriends + girlFriends] / age}`)
+
+console.log (`My name is ${firstName} ${middleInitial} ${lastName}`)
