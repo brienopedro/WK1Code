b0a156e297eb425efb7ba13c008952114e75575b
Pedro Brieno <brienopedro@gmail.com>
2020-06-05 18:54:19 -0700

    added second variables for age and names

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ var guyFriends = 13;
 var girlFriends = 12;
 
 var age = 25;
+var myBrothersAge = 23
 
 var firstName = "Pedro";
 var lastName = 'Brieno';
 var middleInitial = "A.";
 
+var myBrothersFirstName = 'David';
+var myBrothersLastName = 'Brieno';
+var myBrothersMiddleInitial = 'I.';
+
 console.log ('Hello World!');
 
-console.log (`My new balance is ${cash - [pizza * 3 + soda * 5]} after buying three pizza's and five sodas from Little Caesars`)
+console.log (`My new balance is ${cash - [pizza * 3 + soda * 5]} after buying three pizza's and five sodas from Little Caesars`);
 
-console.log (`The number of friends I have made each year is ${[guyFriends + girlFriends] / age}`)
+console.log (`The number of friends I have made each year is ${[guyFriends + girlFriends] / age}`);
 
-console.log (`My name is ${firstName} ${middleInitial} ${lastName}`)
+console.log (`My name is ${firstName} ${middleInitial} ${lastName}`);
